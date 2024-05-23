b2476d4987bdc0d156b6428aa35cf8c6e1991c27
ZemoChen <zeming.chen@dmall.com>
2024-05-23 15:55:47 +0800

    test for json

--- hello_word/main.go
+++ hello_word/main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-    "context"
+	"context"
 
-    "github.com/cloudwego/hertz/pkg/app"
-    "github.com/cloudwego/hertz/pkg/app/server"
-    "github.com/cloudwego/hertz/pkg/protocol/consts"
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
 )
 
 func main() {
-    h := server.Default()
+	h := server.Default()
 
-    h.GET("/hello", func(c context.Context, ctx *app.RequestContext) {
-		ctx.Data(consts.StatusOK,consts.MIMETextPlain, []byte("Hello World!"))
-            // ctx.JSON(consts.StatusOK, utils.H{"message": "pong"})
-    })
+	h.GET("/hello", func(c context.Context, ctx *app.RequestContext) {
+		// ctx.Data(consts.StatusOK,consts.MIMETextPlain, []byte("Hello World!"))
+		ctx.JSON(consts.StatusOK, utils.H{"message": "Hello World!"})
+	})
 
-    h.Spin()
+	h.Spin()
 }
