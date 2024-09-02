a097da5f03fb59d89ff5a352243e508be2709c65
zemo <zeming.chen@dmall.com>
2024-09-02 17:13:58 +0800

    fix: :bug: fix userKey build error

--- app/frontend/utils/function.go
+++ app/frontend/utils/function.go
@@ -3,7 +3,7 @@ package utils
 import "context"
 
 func GetUserIdFromCtx(ctx context.Context) uint32 {
-	userId := ctx.Value(SessionUserId)
+	userId := ctx.Value(UserIdKey)
 	if userId == nil {
 		return 0
 	}
